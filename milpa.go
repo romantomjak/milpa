b7557dc1ebc2f691c05ceb29d8e29691f7882ddb
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2018-09-04 21:18:23 +0100

    go fmt

--- milpa.go
+++ milpa.go
@@ -1,62 +1,62 @@
 package main
 
 import (
-    "bytes"
-    "fmt"
-    "os"
-    "strings"
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
 )
 
 var CODES = map[string]string{
-    "A": "Alpha",
-    "B": "Bravo",
-    "C": "Charlie",
-    "D": "Delta",
-    "E": "Echo",
-    "F": "Foxtrot",
-    "G": "Golf",
-    "H": "Hotel",
-    "I": "India",
-    "J": "Juliett",
-    "K": "Kilo",
-    "L": "Lima",
-    "M": "Mike",
-    "N": "November",
-    "O": "Oscar",
-    "P": "Papa",
-    "Q": "Quebec",
-    "R": "Romeo",
-    "S": "Sierra",
-    "T": "Tango",
-    "U": "Uniform",
-    "V": "Victor",
-    "W": "Whiskey",
-    "X": "X-ray",
-    "Y": "Yankee",
-    "Z": "Zulu",
+	"A": "Alpha",
+	"B": "Bravo",
+	"C": "Charlie",
+	"D": "Delta",
+	"E": "Echo",
+	"F": "Foxtrot",
+	"G": "Golf",
+	"H": "Hotel",
+	"I": "India",
+	"J": "Juliett",
+	"K": "Kilo",
+	"L": "Lima",
+	"M": "Mike",
+	"N": "November",
+	"O": "Oscar",
+	"P": "Papa",
+	"Q": "Quebec",
+	"R": "Romeo",
+	"S": "Sierra",
+	"T": "Tango",
+	"U": "Uniform",
+	"V": "Victor",
+	"W": "Whiskey",
+	"X": "X-ray",
+	"Y": "Yankee",
+	"Z": "Zulu",
 }
 
 func LetterToCode(letter string) string {
-    code := letter
-    if strings.ToLower(letter) == letter {
-        code = strings.ToUpper(letter)
-    }
-    if val, ok := CODES[code]; ok {
-        return val
-    }
-    return code
+	code := letter
+	if strings.ToLower(letter) == letter {
+		code = strings.ToUpper(letter)
+	}
+	if val, ok := CODES[code]; ok {
+		return val
+	}
+	return code
 }
 
 func WordToCode(word string) string {
-    var buffer bytes.Buffer
+	var buffer bytes.Buffer
 	for index, character := range word {
 		letter := string(character)
-		if letter == " " {  // don't process spaces
+		if letter == " " { // don't process spaces
 			continue
 		}
 		code := LetterToCode(letter)
 		space := " "
-		if index+1 == len(word) {  // skip trailing space
+		if index+1 == len(word) { // skip trailing space
 			space = ""
 		}
 		buffer.WriteString(code + space)
@@ -65,14 +65,13 @@ func WordToCode(word string) string {
 }
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Printf("usage: %s hello world\n", os.Args[0])
-        os.Exit(1)
-    }
+	if len(os.Args) < 2 {
+		fmt.Printf("usage: %s hello world\n", os.Args[0])
+		os.Exit(1)
+	}
 
-    for i := 1; i < len(os.Args); i++ {
-        word := os.Args[i]
-        fmt.Println(WordToCode(word))
-    }
+	for i := 1; i < len(os.Args); i++ {
+		word := os.Args[i]
+		fmt.Println(WordToCode(word))
+	}
 }
-
--- milpa_test.go
+++ milpa_test.go
@@ -1,50 +1,50 @@
 package main
 
 import (
-    "strings"
-    "testing"
+	"strings"
+	"testing"
 )
 
 func Test_Maps_Letters_To_Codes(t *testing.T) {
-    for letter, code := range CODES {
-        result := LetterToCode(letter)
-        if code != result {
-            t.Errorf("Expected '%s' to be a '%s' but got '%s'", letter, code, result)
-        }
-    }
+	for letter, code := range CODES {
+		result := LetterToCode(letter)
+		if code != result {
+			t.Errorf("Expected '%s' to be a '%s' but got '%s'", letter, code, result)
+		}
+	}
 }
 
 func Test_Ignores_Unknown_Symbols(t *testing.T) {
-    symbols := []string{" ", ",", ";", "!"}
-    for _, symbol := range symbols {
-        result := LetterToCode(symbol)
-        if symbol != result {
-            t.Errorf("Expected '%s' to be the same, but got '%s'", symbol, result)
-        }
-    }
+	symbols := []string{" ", ",", ";", "!"}
+	for _, symbol := range symbols {
+		result := LetterToCode(symbol)
+		if symbol != result {
+			t.Errorf("Expected '%s' to be the same, but got '%s'", symbol, result)
+		}
+	}
 }
 
 func Test_Ignores_Case(t *testing.T) {
-    for letter, code := range CODES {
-        lcLetter := strings.ToLower(letter)
-        result := LetterToCode(lcLetter)
-        if code != result {
-            t.Errorf("Expected '%s' to be a '%s' but got '%s'", lcLetter, code, result)
-        }
-    }
+	for letter, code := range CODES {
+		lcLetter := strings.ToLower(letter)
+		result := LetterToCode(lcLetter)
+		if code != result {
+			t.Errorf("Expected '%s' to be a '%s' but got '%s'", lcLetter, code, result)
+		}
+	}
 }
 
 func Test_Maps_Word_To_Codes(t *testing.T) {
-    testCases := []struct {
-        words string
-        want  string
-    }{
-        {"Foo", "Foxtrot Oscar Oscar"},
-        {"Foo Bar", "Foxtrot Oscar Oscar Bravo Alpha Romeo"},
-    }
-    for _, tc := range testCases {
-        if got := WordToCode(tc.words); got != tc.want {
-            t.Errorf("Expected '%s' to be a '%s' but got '%s'", tc.words, tc.want, got)
-        }
-    }
+	testCases := []struct {
+		words string
+		want  string
+	}{
+		{"Foo", "Foxtrot Oscar Oscar"},
+		{"Foo Bar", "Foxtrot Oscar Oscar Bravo Alpha Romeo"},
+	}
+	for _, tc := range testCases {
+		if got := WordToCode(tc.words); got != tc.want {
+			t.Errorf("Expected '%s' to be a '%s' but got '%s'", tc.words, tc.want, got)
+		}
+	}
 }
