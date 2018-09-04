0439e2d60ce114e1370297960146e001ee945279
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2018-09-04 19:47:43 +0100

    refactor. use call codes from production code

--- milpa_test.go
+++ milpa_test.go
@@ -4,37 +4,8 @@ import (
     "testing"
 )
 
-var TEST_CODES = map[string]string{
-    "A": "Alpha",
-    "B": "Bravo",
-    "C": "Charlie",
-    "D": "Delta",
-    "E": "Echo",
-    "F": "Foxtrot",
-    "G": "Golf",
-    "H": "Hotel",
-    "I": "India",
-    "J": "Juliett",
-    "K": "Kilo",
-    "L": "Lima",
-    "M": "Mike",
-    "N": "November",
-    "O": "Oscar",
-    "P": "Papa",
-    "Q": "Quebec",
-    "R": "Romeo",
-    "S": "Sierra",
-    "T": "Tango",
-    "U": "Uniform",
-    "V": "Victor",
-    "W": "Whiskey",
-    "X": "X-ray",
-    "Y": "Yankee",
-    "Z": "Zulu",
-}
-
 func Test_Maps_Letters_To_Codes(t *testing.T) {
-    for letter, code := range TEST_CODES {
+    for letter, code := range CODES {
         result := LetterToCode(letter)
         if code != result {
             t.Errorf("Expected '%s' to be a '%s' but got '%s'", letter, code, result)
