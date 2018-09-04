237139907ab7e19d78fb7755aa20a720ba387cc2
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2018-09-04 19:39:57 +0100

    assert codes all alphabet letters

--- milpa_test.go
+++ milpa_test.go
@@ -4,12 +4,41 @@ import (
     "testing"
 )
 
-func Test_Maps_Letter_To_Code(t *testing.T) {
-    letter := "R"
-    code := "Romeo"
-    result := LetterToCode(letter)
-    if code != result {
-        t.Errorf("Expected '%s' to be '%s', but got '%s'", letter, code, result)
+var TEST_CODES = map[string]string{
+    "A": "Alpha",
+    "B": "Bravo",
+    "C": "Charlie",
+    "D": "Delta",
+    "E": "Echo",
+    "F": "Foxtrot",
+    "G": "Golf",
+    "H": "Hotel",
+    "I": "India",
+    "J": "Juliett",
+    "K": "Kilo",
+    "L": "Lima",
+    "M": "Mike",
+    "N": "November",
+    "O": "Oscar",
+    "P": "Papa",
+    "Q": "Quebec",
+    "R": "Romeo",
+    "S": "Sierra",
+    "T": "Tango",
+    "U": "Uniform",
+    "V": "Victor",
+    "W": "Whiskey",
+    "X": "X-ray",
+    "Y": "Yankee",
+    "Z": "Zulu",
+}
+
+func Test_Maps_Letters_To_Codes(t *testing.T) {
+    for letter, code := range TEST_CODES {
+        result := LetterToCode(letter)
+        if code != result {
+            t.Errorf("Expected '%s' to be a '%s' but got '%s'", letter, code, result)
+        }
     }
 }
 
