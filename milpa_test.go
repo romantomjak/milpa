dd145ddfa6d3fd57d567f8acdb1ba009b44e457c
Roman Tomjak <6570684+romantomjak@users.noreply.github.com>
2018-09-08 17:46:12 +0100

    refactor. remove redundant tests

--- milpa_test.go
+++ milpa_test.go
@@ -1,18 +1,8 @@
 package main
 
 import (
-	"strings"
 	"testing"
 )
-
-func Test_Maps_Letters_To_Codes(t *testing.T) {
-	for letter, code := range CODES {
-		result := LetterToCode(letter)
-		if code != result {
-			t.Errorf("Expected '%s' to be a '%s' but got '%s'", letter, code, result)
-		}
-	}
-}
 
 func Test_Ignores_Unknown_Symbols(t *testing.T) {
 	symbols := []string{" ", ",", ";", "!"}
@@ -20,16 +10,6 @@ func Test_Ignores_Unknown_Symbols(t *testing.T) {
 		result := LetterToCode(symbol)
 		if symbol != result {
 			t.Errorf("Expected '%s' to be the same, but got '%s'", symbol, result)
-		}
-	}
-}
-
-func Test_Ignores_Case(t *testing.T) {
-	for letter, code := range CODES {
-		lcLetter := strings.ToLower(letter)
-		result := LetterToCode(lcLetter)
-		if code != result {
-			t.Errorf("Expected '%s' to be a '%s' but got '%s'", lcLetter, code, result)
 		}
 	}
 }
